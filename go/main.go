555b85d3c5ff3295a5989d58e235feff9e95bb96
Mikhail Fadeev <m.fadeew@gmail.com>
2019-09-16 13:37:02 +0300

    first commit

---
+++ go/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"../fins"
+)
+
+
+func main() {
+	plcAddr := "192.168.250.1:9600"
+
+	robot := fins.NewClient(plcAddr)
+	defer robot.CloseConnection()
+
+	//degrees := readCurrentDegrees(robot)
+	//for i,_ := range degrees{
+	//	degrees[i] += 10
+	//}
+	//ONControlMode(robot)
+	//sendPackage(robot,PacToUint([4]float64{-100.0,-100.0,-100.0,-100.0}))
+	//sendPackage(robot,PacToUint(degrees))
+	//OFFControlMode(robot)
+	consolePositionControlWithSmoothMoving(robot)
+}
---
+++ go/send.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"./fins"
+	"log"
+	"strings"
+)
+
+
+/*
+Function read set on engels from file and send they to robot
+*/
+func sendDegreesFromFile (robot *fins.Client, fileName string) {
+	fileLines := strings.Split(ReadAll(fileName), "\r\n")
+	for i := 0; i < len(fileLines); i++ {
+		setAngles(robot, LineToAngles(fileLines[i]))
+	}
+}
+
+/*
+Functions send package to robot
+ */
+func sendPackage(robot *fins.Client, pac []uint16)  {
+	err := robot.WriteDNoResponse(420,pac)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+Function send 4 angles to robot
+ */
+func setAngles(robot *fins.Client, angles [4]float64) {
+	sendPackage(robot,PacToUint(angles))
+}
+
+/*
+Function convert angels package to robot package
+ */
+func PacToUint(pac [4]float64) []uint16 {
+	res := make([]uint16, 4*len(pac))
+
+	for i := 0; i < len(pac); i++ {
+		current := Float64Uint16(pac[i])
+		for j := 0; j < len(current); j++ {
+			res[4*i + j] = current[j]
+		}
+	}
+	return res
+}
